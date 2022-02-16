a7a82a1b723ab4cda27e176e0b772cc9bbbcb9c1
RoanWassink <45005234+RoanWassink@users.noreply.github.com>
2022-02-16 11:30:14 +0100

    Scan Albireo's + update
    -added logging to file
    -added IP scan: usage aip

--- main - kopie.py
+++ main - kopie.py
@@ -4,53 +4,11 @@ from datetime import datetime
 import time
 from itertools import repeat
 import logging
-import subprocess
-import ipaddress
+
 import yaml
 from netmiko import ConnectHandler
 from netmiko.ssh_exception import NetMikoAuthenticationException
 from netmiko.ssh_exception import NetMikoTimeoutException
-
-# Prompt the user to input a network address
-def aipFinder(network, subnet):
-
-    net_addr = network + subnet
-
-    # Create the network
-    ip_net = ipaddress.ip_network(net_addr)
-
-    # Get all hosts on that network
-    all_hosts = list(ip_net.hosts())
-
-    # Configure subprocess to hide the console window
-    info = subprocess.STARTUPINFO()
-    info.dwFlags |= subprocess.STARTF_USESHOWWINDOW
-    info.wShowWindow = subprocess.SW_HIDE
-
-    device_type = "linux"
-    username = "root"
-    password = ''
-    file = open(r'devices.yaml', 'w')
-
-    # For each IP address in the subnet,
-    # run the ping command with subprocess.popen interface
-    for i in range(len(all_hosts)):
-        output = subprocess.Popen(['ping', '-n', '1', '-w', '500', str(all_hosts[i])], stdout=subprocess.PIPE,
-                                  startupinfo=info).communicate()[0]
-
-        if "Destination host unreachable" in output.decode('utf-8'):
-            print(str(all_hosts[i]), "is Offline")
-        elif "Request timed out" in output.decode('utf-8'):
-            print(str(all_hosts[i]), "is Offline")
-        else:
-            print(str(all_hosts[i]), "is Online")
-            dict_file = [{"device_type": device_type, "host": str(all_hosts[i]), "username": "root", "global_delay_factor": 4, "password": password}]
-            documents = yaml.dump(dict_file, file)
-
-    return ''
-    ######################################################################################################################################################
-
-
 netmiko_exceptions = (NetMikoTimeoutException,
                      NetMikoAuthenticationException)
 logging.getLogger("paramiko").setLevel(logging.WARNING)
@@ -70,12 +28,11 @@ received_msg = '<=== {} Received: {}'
 def send_show(device_dict, command):
 
     ip = device_dict['host']
-
     logging.info(start_msg.format(datetime.now().time(), ip, command))
     if ip == '192.168.100.1': time.sleep(5)
     with ConnectHandler(**device_dict) as ssh:
         ssh.enable()
-        result = ssh.send_config_set(command)
+        result = ssh.send_command(command)
         logging.debug(received_msg.format(datetime.now().time(), ip, command))
     return {ip: result[0:10]}
 
@@ -99,19 +56,19 @@ tftp = '172.16.201.70'
 
 
 if __name__ == '__main__':
-    aipFinder('172.16.201.64', '/26')
     with open('devices.yaml') as f:
         devices = yaml.safe_load(f)
+    with open('commands.yaml') as r:
+        command_dict = yaml.safe_load(r)
+        command_single = command_dict['command']
 
-
-    commands = ['tftp -gr blup.sh 172.16.201.70', 'echo tftphost=172.16.201.70 > /tmp/serverparams.conf', 'chmod 0777 blup.sh', './blup.sh']
 
     #pprint(send_command_to_devices(devices, 'ip addr'))
     #pprint(send_command_to_devices(devices, 'tftp -gr blup.sh 172.16.201.70'))
     #pprint(send_command_to_devices(devices, 'echo tftphost=172.16.201.70 > /tmp/serverparams.conf'))
     #pprint(send_command_to_devices(devices, 'chmod 0777 blup.sh'))
     #pprint(send_command_to_devices(devices, './blup.sh'))
-    pprint(send_command_to_devices(devices, commands))
+    pprint(send_command_to_devices(devices, command_dict))
 
 
 
